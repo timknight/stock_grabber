240bf30d7fe0f8ab6cb49d5336cf86ad9cfca417
Tim Knight <tim.knight@acst.org>
2011-02-08 22:35:06 -0500

    Initial Commit

---
+++ stock_grabber.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+class StockGrabber
+  require 'net/http'
+
+  def initialize(stock)
+    @pricesheet = Net::HTTP.get('download.finance.yahoo.com', '/d/quotes.csv?s='+stock+'&f=snl1d1t1c1ohgv&e=.csv').chop.split(",")
+  end
+  
+  def symbol
+    @pricesheet[0].gsub("\"","")
+  end
+  
+  def company
+    @pricesheet[1].gsub("\"","")
+  end
+  
+  def last_traded_price
+    @pricesheet[2]
+  end
+  
+  def last_traded_date
+    @pricesheet[3].gsub("\"","")
+  end
+  
+  def last_traded_time
+    @pricesheet[4].gsub("\"","")
+  end
+  
+  def change
+    @pricesheet[5]
+  end
+  
+  def opening_price
+    @pricesheet[6]
+  end
+  
+  def days_high
+    @pricesheet[7]
+  end
+  
+  def days_low
+    @pricesheet[8]
+  end
+  
+  def volume
+    @pricesheet[9]
+  end
+  
+  def exchange
+    case
+      when self.symbol[-2,2] == ".M"   then "Montreal"
+      when self.symbol[-2,2] == ".V"   then "Vancouver"
+      when self.symbol[-3,3] == ".TO"  then "Toronto"
+      when self.symbol[-3,3] == ".AL"  then "Alberta"
+      else "US"
+    end
+  end
+  
+end
+
+# --------------------------------------------------
+# This is a demo execution of the StockGrabber class
+# --------------------------------------------------
+stock = StockGrabber.new("GOOG")
+puts stock.symbol               # => GOOG
+puts stock.company              # => GOOGLE
+puts stock.last_traded_price    # => 529.64
+puts stock.last_traded_date     # => 2/15/2008
+puts stock.last_traded_time     # => 4:00pm
+puts stock.change               # => -2.61
+puts stock.opening_price        # => 529.98
+puts stock.days_high            # => 532.66
+puts stock.days_low             # => 524.33
+puts stock.volume               # => 5201028
+puts stock.exchange             # => US
